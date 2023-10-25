04e8d171e395130ca5b0034c9ff8c568d8107031
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-25 13:47:38 -0700

    Separate concerns, add test db Test log in and sign up both separately and together.

--- tests/auth-control-test.js
+++ tests/auth-control-test.js
@@ -3,23 +3,36 @@ const index = require('../routes/users');
 const request = require('supertest');
 const express = require('express');
 const app = express();
+require('../tests/mongoConfigTesting');
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/', index);
 
-test('sign up routes works', (done) => {
+test('sign up route works', (done) => {
   request(app)
     .post('/sign-up')
     .type('form')
     .send({
-      username: '',
-      password: '',
-      confirm_password: ''
+      username: 'nick',
+      password: 'PaSsWord1!',
+      confirm_password: 'PaSsWord1!'
+    })
+    .expect(200, done);
+});
+
+test('sign up to log in route works', (done) => {
+  request(app)
+    .post('/sign-up')
+    .type('form')
+    .send({
+      username: 'nick',
+      password: 'PaSsWord1!',
+      confirm_password: 'PaSsWord1!'
     })
     .then(() => {
       request(app).post('/log-in').type('form').send({
-        username: '',
-        password: ''
+        username: 'nick',
+        password: 'PaSsWord1!'
       });
     })
     .expect(200, done);
@@ -30,8 +43,8 @@ test('log in route works', (done) => {
     .post('/log-in')
     .type('form')
     .send({
-      username: '',
-      password: ''
+      username: 'nick',
+      password: 'PaSsWord1!'
     })
     .expect(200, done);
 });
