47cf4701860fa0d7d95573ab99601441aa0017d4
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 09:54:42 -0700

    Require mongo config

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv').config();
+require('./mongoConfig');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
