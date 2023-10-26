c7a2b905ffdcdc31e9362362a91f79371c1dfbe6
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 12:41:19 -0700

    Create middleware

---
+++ verifyJWT.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+function verify(req, res, next) {
+  let token = req.headers['authorization'];
+  token = token.replace(/^Bearer\s+/, '');
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        return res.json({
+          success: false,
+          msg: 'Token is not valid'
+        });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    return res.json({
+      success: false,
+      msg: 'Token not provided'
+    });
+  }
+}
+
+export default verify;
