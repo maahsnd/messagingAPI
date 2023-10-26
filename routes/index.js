327ad8dc510a4a1244c83ffbfd6cf42f1e1731e0
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 12:23:41 -0700

    Format controller as js not jsx

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/auth-controller.jsx');
+const authController = require('../controllers/auth-controller.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
