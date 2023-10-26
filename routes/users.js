1d333465792bc74ef7bca32cd59685192f1417a8
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-26 12:45:34 -0700

    Add jwt verify middleware

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/user-controller');
+const Jwt = require('../verifyJWT');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:id', Jwt.verify, userController.view_user);
+
+router.post('/:id', Jwt.verify, userController.edit_user);
 
 module.exports = router;
