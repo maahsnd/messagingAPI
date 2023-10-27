310329af24b7db6e67e79b05b8771b2e08a0f11e
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 10:45:51 -0700

    Create new message route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user-controller');
+const msgController = require('../controllers/msg-controller');
 const Jwt = require('../verifyJWT');
 
 /* GET users listing. */
 router.get('/:username', Jwt.verify, userController.view_user);
 
 router.post('/:username', Jwt.verify, userController.edit_user);
+
+router.post('/:username/messages', Jwt.verify, msgController.new_message);
 
 module.exports = router;
