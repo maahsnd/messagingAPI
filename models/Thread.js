87a78e6df3b227343bc6309dd6a2694c55c8d9de
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 09:35:07 -0700

    Allow empty threads Avoid circular dependency between messages and threads, both of which required the existence of the other to be created.

--- models/Thread.js
+++ models/Thread.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const ThreadSchema = new Schema({
   users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
-  messages: [{ type: Schema.Types.ObjectId, ref: 'Message', required: true }],
+  messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
   name: { type: Text, minLength: 1, maxLength: 50, required: false }
 });
 
