3953fd674cf3733786a54b27e34a746c3ef25f58
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 13:06:47 -0700

    Use proper type Text not a type.

--- models/Thread.js
+++ models/Thread.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const ThreadSchema = new Schema({
   users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
   messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
-  name: { type: Text, minLength: 1, maxLength: 50, required: false }
+  name: { type: String, minLength: 1, maxLength: 50, required: false }
 });
 
 module.exports = mongoose.model('Thread', ThreadSchema);
