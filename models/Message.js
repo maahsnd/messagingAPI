ee74988100db64da79482c17475f5344faf2688b
mrwindupbirdesq <nmaahs11@gmail.com>
2023-10-27 09:32:27 -0700

    Allow multiple recipients

--- models/Message.js
+++ models/Message.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const MessageSchema = new Schema({
   text: { type: String, required: true },
   from: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  to: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  to: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
   timestamp: { type: Date, default: Date.now() },
   thread: { type: Schema.Types.ObjectId, ref: 'Thread', required: true }
 });
