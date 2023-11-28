ffb454837fead45bb54306a5dda9239468323b2f
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-28 11:50:40 -0800

    Pass function, do not call

--- models/Message.js
+++ models/Message.js
@@ -6,7 +6,7 @@ const MessageSchema = new Schema({
   text: { type: String, required: true },
   from: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   to: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
-  timestamp: { type: Date, default: Date.now() },
+  timestamp: { type: Date, default: Date.now },
   thread: { type: Schema.Types.ObjectId, ref: 'Thread', required: true }
 });
 
