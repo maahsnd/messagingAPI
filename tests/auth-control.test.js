6df052ec3aba483793807f6bc14de7c76d21af40
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-28 08:45:48 -0800

    Push uncommited testing file

--- tests/auth-control.test.js
+++ tests/auth-control.test.js
@@ -4,39 +4,39 @@ const request = require('supertest');
 const express = require('express');
 const User = require('../models/User');
 const app = express();
-require('../tests/mongoConfigTesting');
+require('dotenv').config();
+require('../mongoConfig');
 
 app.use(express.urlencoded({ extended: false }));
 app.use('/', index);
 
-test.only('sign up route works', (done) => {
+test('sign up route works', (done) => {
   request(app)
     .post('/sign-up')
     .type('form')
     .send({
-      username: 'nick',
-      password: 'PaSsWord1!',
-      confirm_password: 'PaSsWord1!'
+      username: 'nick1',
+      password: 'PaSsWord11!',
+      confirm_password: 'PaSsWord11!'
     })
     .expect(200, done);
 });
 
 test('sign up to log in route works', (done) => {
-  request(app)
-    .post('/sign-up')
-    .type('form')
-    .send({
-      username: 'nick',
-      password: 'PaSsWord1!',
-      confirm_password: 'PaSsWord1!'
-    })
-    .then(() => {
-      request(app).post('/log-in').type('form').send({
+  request(app).post('/sign-up').type('form').send({
+    username: 'nick',
+    password: 'PaSsWord1!',
+    confirm_password: 'PaSsWord1!'
+  }),
+    request(app)
+      .post('/log-in')
+      .type('form')
+      .send({
         username: 'nick',
         password: 'PaSsWord1!'
-      });
-    })
-    .expect(200, done);
+      })
+
+      .expect(200, done);
 });
 
 test('log in route works', (done) => {
