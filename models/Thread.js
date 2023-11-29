2f248f9fa0a9f3bedf33545ca77ac13fc122690a
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-29 09:35:13 -0800

    Add time to threads, sort descending on fetch

--- models/Thread.js
+++ models/Thread.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 const ThreadSchema = new Schema({
   users: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
   messages: [{ type: Schema.Types.ObjectId, ref: 'Message' }],
-  name: { type: String, minLength: 1, maxLength: 50, required: false }
+  name: { type: String, minLength: 1, maxLength: 50, required: false },
+  time: {type: Date, default: Date.now}
 });
 
 module.exports = mongoose.model('Thread', ThreadSchema);
