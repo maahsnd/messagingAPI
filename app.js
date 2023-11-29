e7836fe8b8d6f9fcfbd25992e63e1c4b0501b23b
mrwindupbirdesq <nmaahs11@gmail.com>
2023-11-29 15:11:34 -0800

    Trust proxy

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(logger('dev'));
 app.use(express.json());
